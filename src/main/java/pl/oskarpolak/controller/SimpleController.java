8cb6b649ade53cf729a8ae7603c793fa9a4e34eb
Oskar Pol <oskarbank@gmail.com>
2017-02-18 16:26:21 +0100

    Initial commit

---
+++ src/main/java/pl/oskarpolak/controller/SimpleController.java
@@ -0,0 +1,49 @@
+package pl.oskarpolak.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.oskarpolak.model.Email;
+
+/**
+ * Created by OskarPraca on 2017-02-18.
+ */
+
+@Controller
+public class SimpleController {
+
+    @RequestMapping("/")
+    public String index(Model model){
+        model.addAttribute("name", "Oskar");
+        model.addAttribute("isBanned", false);
+        return "start";
+    }
+
+    @RequestMapping(value = "/send", method = RequestMethod.POST)
+    public String send(@RequestParam(value = "text", required = true) String text,
+                       @RequestParam(value = "email", required = true) String email, Model model){
+        model.addAttribute("email", email);
+        return "result";
+    }
+
+    @RequestMapping(value = "/send", method = RequestMethod.GET)
+    public String sendGet(Model model){
+        model.addAttribute("email", "TO NIE TA METODA, NIE MOŻESZ TAK TU WCHODZIC!");
+        return "result";
+    }
+
+    @RequestMapping(value = "/newform", method = RequestMethod.GET)
+    public String newForm(Model model){
+        model.addAttribute("emailClass", new Email());
+        return "newform";
+    }
+
+    @RequestMapping(value = "/newform", method = RequestMethod.POST)
+    public String newFormGet(@ModelAttribute Email email){
+        return "resultEmail";
+    }
+
+}
---
+++ src/main/java/pl/oskarpolak/model/Email.java
@@ -0,0 +1,29 @@
+package pl.oskarpolak.model;
+
+/**
+ * Created by OskarPraca on 2017-02-18.
+ */
+
+public class Email {
+
+    private String email;
+    private String message;
+
+    public Email() { }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
