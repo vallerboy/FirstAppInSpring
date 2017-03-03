f9f73c8519e6b985808122b21425b2086e4cb6b9
Oskar Pol <oskarbank@gmail.com>
2017-03-03 19:58:14 +0100

    REST

--- src/main/java/pl/oskarpolak/controller/APIController.java
+++ src/main/java/pl/oskarpolak/controller/APIController.java
@@ -3,8 +3,10 @@ package pl.oskarpolak.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import pl.oskarpolak.UserRepository;
@@ -24,10 +26,10 @@ public class APIController {
             method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity getUser(@PathVariable("username") String id) {
 
-          User user = userRepository.findByUsername(id);
-          if(user == null) {
-              return new ResponseEntity(HttpStatus.BAD_REQUEST);
-          }
+        User user = userRepository.findByUsername(id);
+        if (user == null) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
 
 //        User user = new User();
 //        user.setPassword("mojehaslo");
@@ -37,5 +39,31 @@ public class APIController {
         return new ResponseEntity(user, HttpStatus.OK);
     }
 
+    @RequestMapping(value = "/api/user", method = RequestMethod.POST)
+    public ResponseEntity addUser(@RequestBody(required = false) User user) {
+        if (user == null) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+        User userDatebase = userRepository.findByUsername(user.getUsername());
+        if (userDatebase != null) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+        user.setPassword(new ShaPasswordEncoder().encodePassword(user.getPassword(), null));
+        userRepository.save(user);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/api/user/{username}",
+            method = RequestMethod.DELETE, produces = "application/json")
+    public ResponseEntity deleteUser(@PathVariable("username") String id) {
+
+        User user = userRepository.findByUsername(id);
+        if (user == null) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+        userRepository.delete(user.getId());
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
 
 }
