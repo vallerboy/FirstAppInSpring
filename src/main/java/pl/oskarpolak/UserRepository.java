8a2a0ee2bf4aed232c21cb2a17eeff1c1c2c9158
Oskar Pol <oskarbank@gmail.com>
2017-03-04 16:41:16 +0100

    REST put method

--- src/main/java/pl/oskarpolak/UserRepository.java
+++ src/main/java/pl/oskarpolak/UserRepository.java
@@ -1,11 +1,27 @@
 package pl.oskarpolak;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import pl.oskarpolak.model.User;
+
+import java.util.List;
 
 /**
  * Created by OskarPraca on 2017-02-28.
  */
 public interface UserRepository extends CrudRepository<User, Integer>{
      User findByUsername(String username);
+     User findByUsernameAndRole(String username, String role);
+     List<User> findByIdGreaterThan(int value);
+     List<User> findByUsernameContaining(String value);
+
+     @Query(value = "SELECT * FROM user WHERE username = ?1", nativeQuery = true)
+     User findByMoj(String username);
+
+     Page<User> findByIdGreaterThan(int value, Pageable pageable);
+
+     // SELECT * FROM users WHERE username LIKE '%value%';
+     // SELECT * FROM users WHERE username = 'asd' AND role = 'asdsd';
 }
--- src/main/java/pl/oskarpolak/controller/SQLController.java
+++ src/main/java/pl/oskarpolak/controller/SQLController.java
@@ -1,12 +1,22 @@
 package pl.oskarpolak.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import pl.oskarpolak.UserRepository;
 import pl.oskarpolak.model.User;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 /**
  * Created by OskarPraca on 2017-03-04.
@@ -21,7 +31,7 @@ public class SQLController {
     @RequestMapping(value = "/api/getUser", method = RequestMethod.GET)
     @ResponseBody
     public String test(){
-        User user = userRepository.findByUsername("oski");
+        Page<User> users = userRepository.findByIdGreaterThan(5, new PageRequest(0, 5, new Sort("username")));
 
         // SELECT * FROM user WHERE username = 'oski';
         // ResultSet (key:value, key:value);
@@ -29,7 +39,42 @@ public class SQLController {
         // user.setUsername(resultSet.get("username"));
         // user.setPassword(resultSet.get("password"));
 
-        return user.toString();
-    }
+        StringBuilder builder = new StringBuilder();
+        for(User user : users.getContent()) {
+          builder.append(user.toString() + "<br>");
+        }
 
+        builder.append("<br><br>");
+
+        builder.append("Wszystkie strony: " + users.getTotalPages() + "<br>");
+        builder.append("Wszystkie elementy: " + users.getTotalElements() + "<br>");
+        builder.append("Ma następną stronę?: " + users.hasNext() + "<br>");
+
+        users.nextPageable();
+
+        return builder.toString();
+    }
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @RequestMapping("/api/mail")
+    public ResponseEntity sendMail(){
+        MimeMessage mail = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+            helper.setTo("marszalek.d@gmail.com");
+            helper.setReplyTo("oskar@microsoft.com");
+            helper.setFrom("oskar@microsoft.com");
+            helper.setSubject("Witaj swiecie");
+            helper.setText("HELLO!", true);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+
+
+        javaMailSender.send(mail);
+        return new ResponseEntity(HttpStatus.OK);
+    }
 }
