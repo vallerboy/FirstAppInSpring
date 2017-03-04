02ce9e58a36cd4181d5652c1b254cb1d11755559
Oskar Pol <oskarbank@gmail.com>
2017-03-04 12:04:32 +0100

    REST put method

--- src/main/java/pl/oskarpolak/controller/APIController.java
+++ src/main/java/pl/oskarpolak/controller/APIController.java
@@ -3,14 +3,16 @@ package pl.oskarpolak.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import pl.oskarpolak.UserRepository;
 import pl.oskarpolak.model.User;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 /**
  * Created by OskarPraca on 2017-03-03.
@@ -63,6 +65,45 @@ public class APIController {
         }
         userRepository.delete(user.getId());
 
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/api/user", method = RequestMethod.PUT)
+    public ResponseEntity updateUser(@RequestBody(required =  false) User user){
+        User userLocal = userRepository.findByUsername(user.getUsername());
+        if(userLocal == null) {
+           new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+
+        userLocal.setUsername(user.getUsername());
+        //userLocal.setPassword(user.getPassword());
+        userLocal.setRole(user.getRole());
+        userRepository.save(userLocal);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+
+    // to poniżej ignore
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @RequestMapping("/api/sendmail")
+    public ResponseEntity sendMail(){
+        MimeMessage mail = javaMailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+            helper.setTo("oskarbank@gmail.com");
+            helper.setReplyTo("oskarbank@gmail.com");
+            helper.setFrom("oskarbank@gmail.com");
+            helper.setSubject("Witaj swiecie");
+            helper.setText("HELLO!", true);
+
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+        javaMailSender.send(mail);
         return new ResponseEntity(HttpStatus.OK);
     }
 
